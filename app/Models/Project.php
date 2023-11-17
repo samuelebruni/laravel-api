9e6f24eaae7df4e5b5d4dbbd79a15c3496815e83
samuelebruni <samuele_bruni@hotmail.com>
2023-11-17 16:33:29 +0100

    initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title','cover_image','description','slug','project_link','online_link', 'type_id'];
+    
+    public function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+    
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
