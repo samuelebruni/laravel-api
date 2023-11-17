41529b985abf7e89edd4149488e3990011ac9b6b
samuelebruni <samuele_bruni@hotmail.com>
2023-11-17 21:01:08 +0100

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('project', function () {
+    return response()->json([
+        'success' => true,
+        'result' => Project::with('type', 'technologies')->orderByDesc('id')->paginate(12)
+    ]);
+});
+
+Route::get('type', function () {
+    return response()->json([
+        'status' => 'success',
+        'result' => App\Models\Type::all()
+    ]);
+});
+
+
+Route::get('technology', function () {
+    return response()->json([
+        'status' => 'success',
+        'result' => App\Models\Technology::all()
+    ]);
 });
